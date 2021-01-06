95befaab3982a2c97309d5e6bce7fe2510e2748d
babakaevdmitry <kapaia999@gmail.com>
2021-01-06 03:39:58 +0300

    add feature and story annotations

--- src/test/java/hw4/tests/firstExerciseTestRun/FirstExerciseRunTest.java
+++ src/test/java/hw4/tests/firstExerciseTestRun/FirstExerciseRunTest.java
@@ -1,10 +1,14 @@
 package hw4.tests.firstExerciseTestRun;
 
 import hw4.tests.abstractTest.BaseTest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 public class FirstExerciseRunTest extends BaseTest {
 
+    @Feature(value = "Проверка домашней страницы")
+    @Story(value = "Все шаги для проверки")
     @Test(description = "Тестирование страницы Home page")
     public void verifyHomePage() {
 
--- src/test/java/hw4/tests/secondExerciseTestRun/SecondExerciseRunTest.java
+++ src/test/java/hw4/tests/secondExerciseTestRun/SecondExerciseRunTest.java
@@ -1,10 +1,14 @@
 package hw4.tests.secondExerciseTestRun;
 
 import hw4.tests.abstractTest.BaseTest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 public class SecondExerciseRunTest extends BaseTest {
 
+    @Feature(value = "Проверка страницы различных элементов")
+    @Story(value = "Все шаги для проверки")
     @Test(description = "Тестирование страницы Different element")
     public void verifyDifferentElementsPage() {
 
